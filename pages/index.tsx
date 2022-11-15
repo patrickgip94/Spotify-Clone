ab01269d05fc0803c31a52352ce0d0045cfbc23f
Patrick Gip <gippatrick@gmail.com>
2022-11-14 19:15:21 -0800

    initial Commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Vibify 2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>This is a spotify clone</h1>
+    </div>
+  )
+}
+
+export default Home;
