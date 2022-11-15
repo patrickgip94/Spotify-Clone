ed3f58bb13f6a1ce54b3cb6ee5843f3f0db3b76e
Patrick Gip <gippatrick@gmail.com>
2022-11-14 21:58:54 -0800

    Next Auth completed

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import { getProviders, signIn } from "next-auth/react";
 
-function login() {
+function Login ({ providers }) {
   return (
-    <div>
-      <h1>This is login page</h1>
+    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
+      <img className="w-64 mb-5" src="https://i.imgur.com/XMOMsmq.png" alt="Logo" />
+
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button 
+            className="bg-[#18D860] text-white p-5 rounded-lg"
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+          >
+            Login with {provider.name}
+          </button>
+        </div>
+      ))}
     </div>
   )
 };
 
-export default login;
+export default Login;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
+}
